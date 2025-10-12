525f029e5642247570b502a5b056a18128eea3aa
gche935 <cuhkgordon@gmail.com>
2025-10-12 15:07:35 +1300

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("  ** Welcome to measureQ  (version 1.6.1) **")
+  packageStartupMessage("  ** Welcome to measureQ  (version 1.6.0) **")
 }
